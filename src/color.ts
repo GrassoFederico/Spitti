a6a54c3e3774b0f1a2e24de42f076abc8d9d24dc
Federico Grasso <gras.federico@gmail.com>
2021-11-10 12:50:03 +0100

    Feat: implemented changeTo() method for color switch in the same instance

--- src/color.ts
+++ src/color.ts
@@ -2,33 +2,51 @@ import { hexToRgb, isValidHex, isValidRgb, rgbToHex } from "./helpers"
 
 export class Color
 {
-    private color: string
-
-    public hex: string
-    public rgb: string
+    private _hex: string|Error
+    private _rgb: string|Error
 
     constructor( color: string )
     {
-        this.color = color
+        this._hex = ''
+        this._rgb = ''
 
-        this.hex = this.getHex()
-        this.rgb = this.getRgb()
+        this.changeTo( color )
+    }
+
+    set hex( color: string )
+    {
+        this._hex = isValidHex( color ).result ? color : rgbToHex( color )
+    }
+
+    set rgb( color: string )
+    {
+        this._rgb = isValidRgb( color ).result ? color : hexToRgb( color )
+    }
+
+    get hex(): string
+    {
+        return ( this._hex instanceof Error ) ? this._hex.message : this._hex
+    }
+
+    get rgb(): string
+    {
+        return ( this._rgb instanceof Error ) ? this._rgb.message : this._rgb
+    }
+
+    changeTo( color: string ): void
+    {
+        this.hex = color
+        this.rgb = color
     }
 
     getHex(): string
     {
-        return isValidHex( this.color ).result ? this.color : this.convert( rgbToHex )
+        return this.hex
     }
 
     getRgb(): string
     {
-        return isValidRgb( this.color ).result ? this.color : this.convert( hexToRgb )
+        return this.rgb
     }
 
-    private convert( callback: CallableFunction ): string
-    {
-        let result = callback( this.color )
-
-        return (result instanceof Error) ? result.message : result
-    }
 }
