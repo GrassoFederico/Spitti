599b1633beab515a1442227720442afc3de6f638
Federico Grasso <gras.federico@gmail.com>
2021-11-11 18:45:10 +0100

    Fix: spitti.ts source file

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import './helpers'
-import './color'
-
-export declare function rgbToHex( rgb: string ): string|Error
-export declare function hexToRgb( hex: string ): string|Error
-export declare function isValidRgb( rgb: string ): Response
-export declare function isValidHex( hex: string ): Response
-
-export declare class Color {}
